a4a61063721c95422f1a9780537624cd5dfa05f9
sikandar173 <sikandaralig96@gmail.com>
2023-05-30 17:32:51 +0530

    initial commit

---
+++ lwc/accountTeamMember/accountTeamMember.js
@@ -0,0 +1,10 @@
+import { LightningElement,wire } from 'lwc';
+//import accountTeamMethod from '@salesforce/apex/AccountTeamHandler.accountTeam';
+export default class AccountTeamMember extends LightningElement {
+
+
+//     @wire(accountTeamMethod)
+//   wireacc;
+
+  
+}
